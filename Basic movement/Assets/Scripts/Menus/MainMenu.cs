fa6f8d10867a55ca6c38a4928b8c9c4ccc1b7029
MetZonderSpek <daniel@realafford.nl>
2022-03-18 15:26:35 +0100

    Made Main Menu
    Only need to code the functions to
    - start game
    - options
    - tutorial

---
+++ Basic movement/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MainMenuUI;
+
+    public void StartGame()
+    {
+        Debug.Log("Start the game!");
+    }
+
+    public void Tutorial()
+    {
+        Debug.Log("Open Tutorial");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Open Options");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit game");
+        Application.Quit();
+    }
+}
