11ad7f2db986b49b0d288378a1cce83bfe238a00
Unknown <JunkmailBelith@gmail.com>
2022-03-19 16:36:07 +0100

    Creating sprites from code works

---
+++ Basic movement/Assets/Scripts/PuzzleDynaScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDynaScript : MonoBehaviour
+{
+    public GameObject recycleSprite;
+    public GameObject gasSprite;
+    public GameObject factorySprite;
+    public GameObject windmillSprite;
+    public GameObject solarSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject testImage = Instantiate(windmillSprite, Vector3.zero, Quaternion.identity) as GameObject;
+        testImage.transform.SetParent(transform, false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
