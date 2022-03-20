24f39fb4dfb0c22d0c0bcdc2935c22d9ae2ce94b
roycruiming <roycruiming@gmail.com>
2022-03-20 17:13:09 +0100

    Map de meent(almost done)
    the first level off the game, It needs a bit more details. We will discuss this in the next meeting

---
+++ Basic movement/Assets/Scripts/CarRide.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRide : MonoBehaviour
+{
+
+    public float angleBetween = 0.0f;
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 targetDir = target.position - transform.position;
+        angleBetween = Vector3.Angle(transform.forward, targetDir);
+    }
+}
