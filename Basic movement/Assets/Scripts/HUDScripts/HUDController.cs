535eae28ab2b800c0c11527d7b340262a6cf327e
mrjokermr <46922309+mrjokermr@users.noreply.github.com>
2022-03-28 15:58:17 +0200

    Verdere basis voor het pop up helper message gezet

--- Basic movement/Assets/Scripts/HUDScripts/HUDController.cs
+++ Basic movement/Assets/Scripts/HUDScripts/HUDController.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class HUDController : MonoBehaviour
 {
+
+    private float popUpDuration;
+
+    GameObject HudCanvas;
+    GameObject PopUpMessageContainer;
+
+
+
+    //constructor
+    void Awake()
+    {
+        this.popUpDuration = 8f; //seconds
+
+        this.HudCanvas = GameObject.FindWithTag("HUDCanvas"); //find the hudcanvas is in the current scene
+        this.PopUpMessageContainer = HudCanvas.transform.Find("PopUpMessageContainer").gameObject; //get the popupmessage container from the hudcanvas
+        this.PopUpMessageContainer.SetActive(false); //disable showing the pop up mesasge container when the scene is initaited
+    }
+
+    public void ShowcaseMessage(string messageText) {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
