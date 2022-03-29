a8a6f26903fb01008fbc87f19624df55832de2cd
roycruiming <roycruiming@gmail.com>
2022-03-29 13:23:58 +0200

    conflicts???

--- Basic movement/Assets/Scripts/DirectionalArrow.cs
+++ Basic movement/Assets/Scripts/DirectionalArrow.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class DirectionalArrow : MonoBehaviour
 {
@@ -12,26 +14,50 @@ public class DirectionalArrow : MonoBehaviour
     public GameObject arrow;
     public Text TextUiCounter;
     public Text GameTimer;
+    public Text GameDone;
+    public Text gameEndScore;
+    public Text gameEndTime;
     private float secondsTimer;
     private int minutemark;
+    public Button back;
 
 
     [System.Obsolete]
+
+
+    private void Awake()
+    {
+        GameDone.text = "";
+        gameEndTime.text = "";
+        gameEndScore.text = "";
+    }
     private void Update()
     {
         //Arrow points to next objective
-        Vector3 targetPosition = target[0].transform.position;
-        targetPosition.y = transform.position.y;
-        transform.LookAt(targetPosition);
+        if (target.Count >= 1)
+        {
+            Vector3 targetPosition = target[0].transform.position;
+            targetPosition.y = transform.position.y;
+            transform.LookAt(targetPosition);   
+            back.gameObject.SetActive(false);
+        }
+        //when game is finnished
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            arrow.transform.position = new Vector3(100, 100, 100);
+            Time.timeScale = 0;
+            GameDone.text = "Gefeliciteerd!";
+            gameEndScore.text = objectivesCounter.ToString() + target.Count;
+            gameEndTime.text = "Tijd = " + minutemark + ":" + Mathf.Round(secondsTimer);
+            back.gameObject.SetActive(true);
+            
+
+        }
+
 
         TextUiCounter.text = objectivesCounter.ToString() + "/5";
 
-        //Destroy if all objects are done
-        if (objectivesCounter == 5)
-        {
-            //arrow.gameObject.active = false;
-            arrow.transform.position = new Vector3(100, 100, 100);
-        }
 
 
         //timer in game.
@@ -42,6 +68,9 @@ public class DirectionalArrow : MonoBehaviour
             minutemark++;
         }
         GameTimer.text = minutemark + ":" + Mathf.Round(secondsTimer);
+
+
+        
     }
 
 
@@ -52,6 +81,8 @@ public class DirectionalArrow : MonoBehaviour
         objectivesCounter++;
 
     }
+
+
 
 
 }
