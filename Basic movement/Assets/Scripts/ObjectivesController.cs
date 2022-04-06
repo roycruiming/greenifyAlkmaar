40198e69dcf87d5aa3146506f35ebf1b88e4df70
roycruiming <roycruiming@gmail.com>
2022-04-06 15:27:30 +0200

    counter ++

--- Basic movement/Assets/Scripts/ObjectivesController.cs
+++ Basic movement/Assets/Scripts/ObjectivesController.cs
@@ -69,6 +69,7 @@ public class ObjectivesController : MonoBehaviour
         {
             if (solarPanelsSpot[i] == null)
                 solarPanelsSpot.RemoveAt(i);
+            objectivesCounter++;
         }
 
         //timer in game.
