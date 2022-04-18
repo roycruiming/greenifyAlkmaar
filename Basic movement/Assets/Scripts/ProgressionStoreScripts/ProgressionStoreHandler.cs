c0874373dbe0ac5c641c5aaf988db7e115595b62
mrjoker <makkertje330@live.nl>
2022-04-18 15:12:34 +0200

    Unlockables can be purchased, characters can be equipped and feedback is shown in the progression store

--- Basic movement/Assets/Scripts/ProgressionStoreScripts/ProgressionStoreHandler.cs
+++ Basic movement/Assets/Scripts/ProgressionStoreScripts/ProgressionStoreHandler.cs
@@ -13,6 +13,8 @@ public class ProgressionStoreHandler : MonoBehaviour
     private int currentPowerUpShopIndex;
     private int currentlySelectedCharacterShopId;
     private int currentlySelectedPowerUpShopId;
+
+    private bool popUpIsBeingShown = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +22,16 @@ public class ProgressionStoreHandler : MonoBehaviour
         this.allCharacterUnlockablesInfo = GlobalGameHandler.GetAllUnlockablesInfoByType(UnlockableType.character);
         
         this.UpdateTotalPlayerCointsUI();
+        this.SetFirstUnlocksDataAndIndex();
+        //updateShowcase(allUnlockablesInfo[0]);
     }
 
     private void SetFirstUnlocksDataAndIndex() {
         this.currentCharacterShopIndex = 0;
         this.currentPowerUpShopIndex = 0;
 
-        updateShowcase(this.allCharacterUnlockablesInfo[currentCharacterShopIndex]);
+        updateShowcase(this.allCharacterUnlockablesInfo[currentCharacterShopIndex]); //set characters
+        //function for setting the first powerup
     }
 
     public void showcaseNextCharacter() {
@@ -51,29 +56,64 @@ public class ProgressionStoreHandler : MonoBehaviour
 
     }
 
-    private void updateShowcase(Unlockable unlockInfo) {
-        GameObject containerElement = null;
-        if(unlockInfo.type == UnlockableType.character) containerElement = GameObject.Find("Characters_Showcase_Container");
-        else if(unlockInfo.type == UnlockableType.powerUp) containerElement = GameObject.Find("PowerUps_Showcase_Container");
-
+    public void updateShowcase(Unlockable unlockInfo) {
+        GameObject containerElement = null, buyButton = null;
+        if(unlockInfo.type == UnlockableType.character) {
+            containerElement = GameObject.Find("Characters_Showcase_Container");
+            buyButton = GameObject.Find("purchase_Char_But");
+        }
+        else if(unlockInfo.type == UnlockableType.powerUp) {
+            containerElement = GameObject.Find("PowerUps_Showcase_Container");
+            buyButton = GameObject.Find("purchase_PowerUp_But");
+        }
+        
         if(containerElement != null) {
             //always set the price, let the purchased info overwrite if needed
             containerElement.transform.Find("coints_amount_text").GetComponent<TextMeshProUGUI>().text = unlockInfo.price.ToString();
             containerElement.transform.Find("level_unlock_text").GetComponent<TextMeshProUGUI>().text = GlobalGameHandler.GetTextByDictionaryKey("unlocked in level") + " " + unlockInfo.unlockedInLevel.ToString();
             containerElement.transform.Find("showcase_image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Unlock_Images/" + unlockInfo.exampleImageName);
+            //always set the corresponding unlockable into the UI button script
+            if(buyButton.GetComponent<UnlockableUiButton>() != null) buyButton.GetComponent<UnlockableUiButton>().unlockAble = unlockInfo;
 
             if(unlockInfo.isPurchased) {
                 containerElement.transform.Find("coints_amount_text").GetComponent<TextMeshProUGUI>().text = GlobalGameHandler.GetTextByDictionaryKey("purchased");
                 containerElement.transform.Find("lock-icon").gameObject.SetActive(false); 
+
+                //change the buy button text to equip
+                if(unlockInfo.type == UnlockableType.character) buyButton.transform.Find("Text").GetComponent<Text>().text = GlobalGameHandler.GetTextByDictionaryKey("select character");
+                //ELSE SELECT POWERUP TEXT ADD LATER ON!!!!
             }
 
             if(unlockInfo.isUnlocked) {
+                containerElement.transform.Find("lock-icon").gameObject.SetActive(false);
+            }
+            else {
                 containerElement.transform.Find("lock-icon").gameObject.SetActive(true);
             }
         }
     }
 
-    private void UpdateTotalPlayerCointsUI() {
+    public void showcasePopUpMessage(string text, float durationInSeconds) {
+        GameObject progressionStorePopUpMessageContainer = GameObject.Find("ProgressionStoreUIContainer").transform.Find("ProgressionStorePopUpMessageContainer").gameObject;
+        progressionStorePopUpMessageContainer.SetActive(true);
+        if(popUpIsBeingShown == false) StartCoroutine(showcasePopupMessage(text, durationInSeconds));
+    }
+
+    IEnumerator showcasePopupMessage(string text, float duration) {
+        popUpIsBeingShown = true;
+        GameObject.Find("PopUpMessageBackground").GetComponent<FadeInOutScript>().StartFading();
+        yield return new WaitForSeconds(0.2f);
+        GameObject.Find("PopUpMessageText").GetComponent<TextMeshProUGUI>().text = text;
+        yield return new WaitForSeconds(duration);
+        GameObject.Find("PopUpMessageText").GetComponent<TextMeshProUGUI>().text = "";
+        GameObject.Find("PopUpMessageBackground").GetComponent<FadeInOutScript>().StartFadingOut();
+        //GameObject.Find("PopUpMessageBackground").GetComponent<FadeInOutScript>().StartFadingOut();
+        yield return new WaitForSeconds(2);
+        popUpIsBeingShown = false;
+
+    }
+
+    public void UpdateTotalPlayerCointsUI() {
         GameObject.Find("playerCointsTotal").GetComponent<TextMeshProUGUI>().text = GlobalGameHandler.GetTotalPlayerCointsAmount().ToString();
     }
     public void purchaseUnlockable(int unlockableId) {
