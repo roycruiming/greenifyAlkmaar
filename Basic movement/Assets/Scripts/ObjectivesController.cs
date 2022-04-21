5103b3565d17b5703e607398be6829b6c7636845
roycruiming <roycruiming@gmail.com>
2022-04-21 09:43:17 +0200

    Last Fixxes

--- Basic movement/Assets/Scripts/ObjectivesController.cs
+++ Basic movement/Assets/Scripts/ObjectivesController.cs
@@ -105,18 +105,18 @@ public class ObjectivesController : MonoBehaviour
 
         this.CheckNextProgressionPhase();
         //when objectivesList == emtpy - game is finished
-        /* if(targets.Count == 0 && solarPanels.Count == 0 && solarPanelsSpot.Count == 0)
+         if(targets.Count == 0 && solarPanels.Count == 0 && solarPanelsSpot.Count == 0)
          {
              Cursor.lockState = CursorLockMode.Confined;
              Cursor.visible = true;
              Time.timeScale = 0;
-             puzzleCanvas.gameObject.SetActive(false);
+             //puzzleCanvas.gameObject.SetActive(false);
 
              blackBarArroundScoreScreen.gameObject.SetActive(true);
-             nameInput.gameObject.SetActive(true);
-             nameInputBar.gameObject.SetActive(true);
+             //nameInput.gameObject.SetActive(true);
+             //nameInputBar.gameObject.SetActive(true);
              GameDone.text = "Gefeliciteerd!";
-             gameEndScore.text = objectivesCounter.ToString() + "/" + totalObjectives;
+             //gameEndScore.text = objectivesCounter.ToString() + "/" + totalObjectives;
              gameEndTime.text = "Tijd = " + minutemark + ":" + Mathf.Round(secondsTimer);
              back.gameObject.SetActive(true);
              gameFinnished = true;
@@ -131,7 +131,7 @@ public class ObjectivesController : MonoBehaviour
                print(analyticsResult);
                analyticsSend = true;
              }
-         }*/
+         }
     }
 
     private void CheckNextProgressionPhase() {
@@ -186,6 +186,6 @@ public class ObjectivesController : MonoBehaviour
         int result = minutemark * 100;
         SubmitScore.AddNewHighscore(nameInput.GetComponent<InputField>().text, result + secondsFinal);
 
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene("Mainmap-Scene");
     }
 }
