61c118477e80ba584a16023b5910232c97a953df
MetZonderSpek <daniel@realafford.nl>
2022-05-22 22:16:11 +0200

    Added analytics to multiplayer level

--- Basic movement/Assets/Scripts/LobbyManager.cs
+++ Basic movement/Assets/Scripts/LobbyManager.cs
@@ -3,17 +3,29 @@ using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
     public TMP_InputField createInput;
     public TMP_InputField joinInput;
     
+    private string levelName = "AZ-Stadion";
+
     //max players per room
     public byte maxPlayers;
 
     public void CreateButton()
     {
+        //Sends the analytics that multiplayer has started
+        AnalyticsResult analyticsResult = Analytics.CustomEvent(
+ 	    "LevelStart",
+        new Dictionary<string, object> {
+            {"level", levelName}
+        });
+
+
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = maxPlayers;
 
