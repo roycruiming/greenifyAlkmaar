f6039be5408449ad25078c5b9236f9b7fb6ff4ed
566810 <timranzijn1@gmail.com>
2022-05-27 19:27:40 +0200

    bug 103

--- Basic movement/Assets/GoToNextScreen.cs
+++ Basic movement/Assets/GoToNextScreen.cs
@@ -6,76 +6,30 @@ using UnityEngine.UI;
 
 public class GoToNextScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
 
-    
 
-     public void start()
-    {
-     
-    }
 
     public void next() {
-
-       // DontDestroyOnLoad(GameObject.Find("OnSceneLoaded"));
         SceneManager.LoadScene(5);
-
     }
 
     public void nextMp()
     {
-
-        // DontDestroyOnLoad(GameObject.Find("OnSceneLoaded"));
         SceneManager.LoadScene(0);
-
     }
 
 
     public void GoToPhotoScene() {
-        string score = GameObject.Find("GameTimer").GetComponent<Text>().text;
-        GameObject OnSceneLoaded = GameObject.Find("OnSceneLoaded");
 
-        if (OnSceneLoaded == null) {
-
-            GameObject pref = Resources.Load("OnSceneLoaded", typeof(GameObject)) as GameObject;
-            pref.gameObject.name = "OnSceneLoaded";
-            DontDestroyOnLoad(pref); 
-
-
-           
-            
-
-
-        }
-
-        SceneLoaded sl = OnSceneLoaded.GetComponent<SceneLoaded>();
-        if (sl != null) sl.TimeScore = score;
-
-
-        GameObject PLAYER = GameObject.Find("3RD Person"); 
-
-
-
-
-        DontDestroyOnLoad(PLAYER);
-        DontDestroyOnLoad(OnSceneLoaded);
-
+        GameObject p = GameObject.Find("3RD Person"); 
+        DontDestroyOnLoad(p);
         SceneManager.LoadScene(18);
 
     }
 
     public void GoToPhotoSceneMultiPlayer()
     {
-        //string score = GameObject.Find("GameTimer").GetComponent<Text>().text;
-        GameObject OnSceneLoaded = GameObject.Find("OnSceneLoaded");
-
-        //OnSceneLoaded.GetComponent<SceneLoaded>().TimeScore = score;
-
-        //Object.DontDestroyOnLoad(GameObject.Find("3RD Person"));
-        //Object.DontDestroyOnLoad(GameObject.Find("OnSceneLoaded"));
-
         SceneManager.LoadScene(19);
-
     }
 
 }
--- Basic movement/Assets/SceneLoaded.cs
+++ Basic movement/Assets/SceneLoaded.cs
@@ -7,13 +7,13 @@ using UnityEngine.UI;
 public class SceneLoaded : MonoBehaviour
 {
     public string TimeScore;
-    //private Scene PreviousScene;
-    //private readonly string[] Levels = {"Tutorial-Level", "DeMeentV2", "DeKaasMarkt"};
     private string previousName = "";
     private bool isFirst = true; 
 
     private void OnEnable()
     {
+
+        print("enabled"); 
         SceneManager.sceneLoaded += onSceneLoaded;
         if (isFirst) {
             isFirst = false;
@@ -22,16 +22,7 @@ public class SceneLoaded : MonoBehaviour
 
     }
 
-    private void Awake()
-    {
-        //onSceneLoaded; 
-    }
 
-    
-
-
-    // Start is called before the first frame update
-    // called second
 
     private void onSceneLoaded(Scene scene, LoadSceneMode mode) {
         print(scene.name);
@@ -47,7 +38,6 @@ public class SceneLoaded : MonoBehaviour
             if(GameObject.Find("LevelTitle") != null)
             {
                 string title = GlobalGameHandler.GetPreviousScneneName();
-
                 if (title == "Tutorial-Level") title = "Tutorial";
                 if (title == "DeMeentv2") title = "De Meent";
                 if (title == "DeKaasmarkt") title = "Kaasmarkt";
@@ -56,56 +46,28 @@ public class SceneLoaded : MonoBehaviour
 
             }
         }
-       
 
-        
+
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
-
         GameObject.Find("GameTimer").GetComponent<Text>().text = TimeScore; 
-
         GameObject player = GameObject.Find("3RD Person");
         if (player == null) GameObject.Find("player"); 
-        
-
         GameObject placeholder = GameObject.Find("player placeholder");
-
-
         foreach (Transform c in placeholder.transform) Destroy(c.gameObject);
-
-
-        //set animator to our custom animator
         Animator animator = player.GetComponent<Animator>();
         animator.applyRootMotion = false;
         animator.runtimeAnimatorController = Resources.Load("ScoreBoardAnim") as RuntimeAnimatorController;
-      
-
-
-
         player.transform.SetParent(placeholder.transform);
         player.transform.localPosition = new Vector3(0, 0, 0);
         player.transform.localRotation = Quaternion.Euler(0, 0, 0);
-
         Destroy(player.GetComponent<ThirtPersonPLayerScript>());
         Destroy(player.GetComponent<ArrowVisible>());
         Destroy(player.GetComponent<raycaster>());
-
-
-
-
         Transform cam = player.transform.Find("Main Camera");
         if (!cam) cam = player.transform.Find("Camera");
-
         if (cam != null) Destroy(cam.gameObject); 
-
-       
-
-
-
-        //player.transform.parent = placeholder.transform;
-
         Debug.Log("socLayer loaded");
-
     }
 
 }
