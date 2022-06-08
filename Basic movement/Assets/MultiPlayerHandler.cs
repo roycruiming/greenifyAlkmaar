404fdac4a7d3796d40ebba1af8cc54a01ffbe545
566810 <timranzijn1@gmail.com>
2022-06-08 19:49:57 +0200

    removed a piece of code that triggers a wincondition on backspace

--- Basic movement/Assets/MultiPlayerHandler.cs
+++ Basic movement/Assets/MultiPlayerHandler.cs
@@ -145,9 +145,7 @@ public class MultiPlayerHandler : MonoBehaviourPunCallbacks, IPunObservable
 
         totalCount = solarCounter + TurbineCounter + treeCounter;
 
-        if (Input.GetKey(KeyCode.Backspace)){
-            WinConditie(); 
-        }
+
 
         if (Input.GetKeyDown("k"))
         {
