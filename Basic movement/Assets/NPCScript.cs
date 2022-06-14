26b3e1a2524af20f3b2af45ccb8fdb5950549670
566810 <timranzijn1@gmail.com>
2022-06-14 16:50:59 +0200

    Revert "some extra comments"
    This reverts commit cac8295159ca64067d4f89c2908c72f8acc75dfb.

--- Basic movement/Assets/NPCScript.cs
+++ Basic movement/Assets/NPCScript.cs
@@ -5,27 +5,32 @@ public class NPCScript : MonoBehaviour
 {
     public float walkAnimationSpeed; 
     public Animator animator;
-    private new Rigidbody rigidbody;
+    public new Rigidbody rigidbody;
     
 
     [SerializeField] private Waypoints waypoints;
     [SerializeField] private float moveSpeed = 8f;
+
+    //the waypoint the target is walking towards
     private Transform currentWaypoint;
+    //the waypoint that functions as the spawn/ startingwaypoint 
     public Transform StartingWaypoint;
+    //a curve is devided up into this amount of points 
     public int amountOfPointsCurve = 50;
+
 
 
     private void Awake()
     {
-        //if no startingwaypoint,  get "next waypoint from waypoints")
+        //If no startingwaypoint is defined, get startingwaypoint from waypoints. 
         if (StartingWaypoint == null)
         {
             currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
         }
-        //else currentwaypoint is starting waypoint
+        //Else set currentwaypoint as starting waypoint 
         else { currentWaypoint = StartingWaypoint; }
 
-        //set npc at start waypoint 
+        //teleport to starting/current waypoint. 
         transform.position = currentWaypoint.position;
     }
 
@@ -33,43 +38,40 @@ public class NPCScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //animation
         animator.SetFloat("Vertical", 1, 0.1f, Time.fixedDeltaTime);
         animator.SetFloat("WalkSpeed", walkAnimationSpeed);
-        //move the object
         Move();
     }
 
+
     private void Move()
     {
-        //if routetype is linear && the currentwaypoint is the first waypoint, teleport the player 
-        //to the first waypoint
+        //  If routetype = linear and the currentwaypoint is the first child in the hierarchy (waypoint is firstwaypont
         if (waypoints.RouteType == RouteType.Linear && waypoints.IsFirstChild(currentWaypoint))
         {
+            //teleport npc to first waypoint. 
             transform.position = currentWaypoint.position;
         }
 
-        //if waypoint is a curve (contains a child), use method movecurve and return
-        if (waypoints.IsCurve(currentWaypoint))
-        {
-            MoveCurve();
-            return;
-        }
+        //if the waypoint has a child, that means that the npc needs to move in a curve. 
+        if (waypoints.IsCurve(currentWaypoint)) MoveCurve();
+        //else use MoveLine method for moving a straight line. 
+        else MoveLine();
+    }
 
-        // else move to next waypoint
-        else
-        {
 
-            LookAtIgnoreY(currentWaypoint.position);
-            rigidbody.MovePosition(Vector3.MoveTowards(this.transform.position, currentWaypoint.position, moveSpeed * Time.fixedDeltaTime));
 
-        }
+    private void MoveLine()
+    {
+        rigidbody.MovePosition(Vector3.MoveTowards(this.transform.position, currentWaypoint.position, moveSpeed * Time.fixedDeltaTime));
 
-        // if waypoint is close set next waypoint
         if (Vector3.Distance(transform.position, currentWaypoint.position) < 0.5)
         {
+
             currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+            LookAt(currentWaypoint.position);
         }
+
     }
 
 
@@ -79,65 +81,47 @@ public class NPCScript : MonoBehaviour
 
     private List<Vector3> curvePoints = new List<Vector3>();
 
+
+    //TODO: IMPROVE THIS CODE.
     private void MoveCurve() {
-        //
+        //first item in the curve is curvecontrol 
         Transform curveControl = currentWaypoint.GetChild(0);
         //initiate curvePoints 
-        
-        //if no points in list 
         if (curvePoints.Count == 0 )
         {
-            //divide line into points
             for (int i = 1; i < amountOfPointsCurve + 1; i++)
             {
-               
-                //not really a percentage but I dont know the word for a number like "0.25" 
-                float CurveProgress = i / (float)amountOfPointsCurve;
-                
-
-                //curve is drawn between the entry (currentwaypoint.position, curvecontrol and the next waypoint)
-                Vector3 pointPos = waypoints.CalculateQuadraticBezierPoint(CurveProgress, currentWaypoint.position, curveControl.position, waypoints.GetNextWayPoint(currentWaypoint).position); 
+                float t = i / (float)amountOfPointsCurve;
+                Vector3 pointPos = waypoints.CalculateQuadraticBezierPoint(t, currentWaypoint.position, curveControl.position, waypoints.GetNextWayPoint(currentWaypoint).position); 
                 curvePoints.Add(pointPos);
             } 
         }
 
-
-        // if point count is above 0 
         else if (curvePoints.Count > 0)
         {
-            //move npc to position
             rigidbody.MovePosition(Vector3.MoveTowards(this.transform.position, curvePoints[0], moveSpeed * Time.fixedDeltaTime));
 
-            //if player is close to point, look at next curvepoint and remove point from list
             if (Vector3.Distance(transform.position, curvePoints[0]) < 0.5)
             {
                 Vector3 lookat = curvePoints[0];
                 curvePoints.RemoveAt(0);
-                LookAtIgnoreY(lookat);
+                LookAt(lookat);
             }
 
-            //if curve is finished (no points left in list), set next waypoint as current 
             if (curvePoints.Count == 0)
             {
                 currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
-                LookAtIgnoreY(currentWaypoint.position); 
+                LookAt(currentWaypoint.position); 
             }
         }
     }
 
-
-
-
-    //look at vector but ignore y-axis 
-    private void LookAtIgnoreY(Vector3 lookat)
+    private void LookAt(Vector3 lookat)
     {
         lookat.y = transform.position.y;
         transform.LookAt(lookat);
         
     }
-
-    
-
 
 
 
